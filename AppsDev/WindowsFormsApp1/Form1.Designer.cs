267da734c3c6124b4b068c462d93ad7ddc63e1a4
MathewJamesDagle <126637284+MathewJamesDagle@users.noreply.github.com>
2023-03-15 16:59:51 +0800

    Add files via upload

---
+++ AppsDev/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.greetingsGen = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.Label();
+            this.firstNameInput = new System.Windows.Forms.TextBox();
+            this.lastNameInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.BackColor = System.Drawing.SystemColors.Info;
+            this.submit.Location = new System.Drawing.Point(117, 302);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(115, 25);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.SystemColors.Info;
+            this.exit.Location = new System.Drawing.Point(134, 359);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(80, 25);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // greetingsGen
+            // 
+            this.greetingsGen.AutoSize = true;
+            this.greetingsGen.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greetingsGen.Location = new System.Drawing.Point(13, 57);
+            this.greetingsGen.Name = "greetingsGen";
+            this.greetingsGen.Size = new System.Drawing.Size(337, 37);
+            this.greetingsGen.TabIndex = 2;
+            this.greetingsGen.Text = "Greetings Generator!";
+            this.greetingsGen.Click += new System.EventHandler(this.greetingsGen_Click);
+            // 
+            // firstName
+            // 
+            this.firstName.AutoSize = true;
+            this.firstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstName.Location = new System.Drawing.Point(59, 121);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(81, 18);
+            this.firstName.TabIndex = 3;
+            this.firstName.Text = "First Name";
+            this.firstName.Click += new System.EventHandler(this.firstName_Click);
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastName.Location = new System.Drawing.Point(59, 196);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(80, 18);
+            this.lastName.TabIndex = 4;
+            this.lastName.Text = "Last Name";
+            this.lastName.Click += new System.EventHandler(this.lastName_Click);
+            // 
+            // firstNameInput
+            // 
+            this.firstNameInput.BackColor = System.Drawing.SystemColors.Info;
+            this.firstNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameInput.Location = new System.Drawing.Point(90, 155);
+            this.firstNameInput.Name = "firstNameInput";
+            this.firstNameInput.Size = new System.Drawing.Size(175, 20);
+            this.firstNameInput.TabIndex = 5;
+            this.firstNameInput.Text = "Enter First Name";
+            this.firstNameInput.TextChanged += new System.EventHandler(this.firstNameInput_TextChanged);
+            // 
+            // lastNameInput
+            // 
+            this.lastNameInput.BackColor = System.Drawing.SystemColors.Info;
+            this.lastNameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameInput.Location = new System.Drawing.Point(90, 229);
+            this.lastNameInput.Name = "lastNameInput";
+            this.lastNameInput.Size = new System.Drawing.Size(175, 20);
+            this.lastNameInput.TabIndex = 6;
+            this.lastNameInput.Text = "Enter Last Name";
+            this.lastNameInput.TextChanged += new System.EventHandler(this.lastNameInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(362, 450);
+            this.Controls.Add(this.lastNameInput);
+            this.Controls.Add(this.firstNameInput);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.greetingsGen);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label greetingsGen;
+        private System.Windows.Forms.Label firstName;
+        private System.Windows.Forms.Label lastName;
+        private System.Windows.Forms.TextBox firstNameInput;
+        private System.Windows.Forms.TextBox lastNameInput;
+    }
+}
+
---
+++ AppsDev/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void greetingsGen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstNameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lastNameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!" + " " + firstNameInput.Text + " " + lastNameInput.Text, "Display", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lastName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
